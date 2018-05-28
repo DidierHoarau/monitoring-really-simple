05582c742ccb877cec7f2dc1502fed0e378a7902
Didier Hoarau <didier.hoarau.dev@gmail.com>
2018-05-29 06:24:36 +0800

    [mrs-events-db] Fix backup

--- mrs-events-db/docker-packaging-config/couchdb-backup-wrapper.sh
+++ mrs-events-db/docker-packaging-config/couchdb-backup-wrapper.sh
@@ -3,4 +3,4 @@
 rm -fr /opt/backups/
 mkdir -p /opt/backups/
 
-/opt/scripts/couchdb-backup.sh -b -H 127.0.0.1 -d mrs -f /opt/backups/mrs.json
+/opt/scripts/couchdb-backup.sh -b -H 127.0.0.1 -d mrs-events -f /opt/backups/mrs-events.json
