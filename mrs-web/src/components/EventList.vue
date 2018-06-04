0e6df5132df63795610e973d9a022c31e5de51e8
Didier Hoarau <didier.hoarau.dev@gmail.com>
2018-06-04 19:41:48 +0800

    [mrs-web] Improve event display

--- mrs-web/src/components/EventList.vue
+++ mrs-web/src/components/EventList.vue
@@ -15,9 +15,9 @@
           <div class="cell small-12 medium-5 large-9">{{ formatDate(event.date) }}</div>
           <div class="cell small-8 medium-auto">{{ event.origin }}</div>
           <div class="cell small-4 medium-shrink text-right"><span class="label primary">{{ event.level }}</span></div>
-          <div class="cell small-12 event_content"><code>{{ event.content }}</code></div>
+          <div class="cell small-12 event_content"><code v-html="formatContent(event.content)"></code></div>
           <div class="cell small-9">
-            <span v-for="tag in event.tags" class="label secondary">{{ tag }}</span>
+            <span v-for="tag in event.tags" class="label secondary spaced-label">{{ tag }}</span>
           </div>
           <div class="cell small-3 text-right">
             <a class="button tiny secondary" href="#" v-on:click="acknowledge(event.id)">ACK</a>
@@ -56,6 +56,12 @@ export default class EventList extends Vue {
     return new Date(dateString).toLocaleString();
   }
 
+  public formatContent(content: string): string {
+    return JSON.stringify(content, null, '______')
+      .replace(/\n/g, '&nbsp;<br />')
+      .replace(/______/g, '&nbsp;&nbsp;');
+  }
+
   public acknowledge(eventId: string): void {
     MrsServer.postEventsAck(eventId).then(() => {
       setTimeout(() => {
@@ -70,5 +76,8 @@ export default class EventList extends Vue {
 <style scoped lang="scss">
 .event_content {
   padding: 1em;
+}
+.spaced-label {
+  margin-right: 0.6em;
 }
 </style>
